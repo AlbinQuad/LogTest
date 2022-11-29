71538a0a483beab6651911d38284a4e28be70796
AlbinQuad <112534223+AlbinQuad@users.noreply.github.com>
2022-11-29 13:09:04 +0530

    Update myfunction.py

--- myfunction.py
+++ myfunction.py
@@ -8,7 +8,7 @@ import datetime
 exepath = (r"D:\Project\node-red\getQueue\getQueue\getQueue\bin\Debug\netcoreapp3.1\getQueue.exe")
 
 def orch_log(task_name,status,log_desc='',log_level='info',screenshot=False, ):
-    log_dict = {'Task Name':task_name,'Log Level':log_level,'Status':status,'Log':log_desc}
+    log_dict = {'TaskName':task_name,'LogLevel':log_level,'Status':status,'Log':log_desc}
     message = json.dumps(log_dict)
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',port=3672))
     channel = connection.channel()
@@ -24,7 +24,7 @@ def create_task(task_name):
     username = platform.node()+'\\'+ os.getlogin( )
     print(username)
     current_time = str(datetime.datetime.utcnow())
-    task_dict = {'Task Name': task_name,'Status':"Pending",'Timespamp':current_time,"SystemName":username} 
+    task_dict = {'TaskName': task_name,'Status':"Pending",'Timespamp':current_time,"SystemName":username} 
     
     return task_dict
 
